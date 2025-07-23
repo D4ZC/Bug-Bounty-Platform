bd5352336a45bfc70dc52e8911e7e6763c8ffd77
Menmabb <mangwhr@protonmail.com>
2025-07-23 08:57:16 -0600

    modulo Language
    Se creo una archivo .jason para cada idioma, pero faltan más palabras de la aplicación.

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,58 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import es from './locales/es.json';
+import pt from './locales/pt.json';
 
 const resources = {
-  en: {
-    translation: {
-      navbar: {
-        title: 'BugBounty',
-      },
-      settings: {
-        title: 'Settings',
-        save: 'Save Changes',
-        letterSize: 'Letter Size',
-        color: 'Color',
-        language: 'Language',
-        addLanguage: 'Add Language',
-        cancel: 'Cancel',
-        add: 'Add',
-      },
-    },
-  },
-  es: {
-    translation: {
-      navbar: {
-        title: 'BugBounty',
-      },
-      settings: {
-        title: 'Configuración',
-        save: 'Guardar Cambios',
-        letterSize: 'Tamaño de letra',
-        color: 'Color',
-        language: 'Idioma',
-        addLanguage: 'Agregar idioma',
-        cancel: 'Cancelar',
-        add: 'Agregar',
-      },
-    },
-  },
-  pt: {
-    translation: {
-      navbar: {
-        title: 'BugBounty',
-      },
-      settings: {
-        title: 'Configurações',
-        save: 'Salvar Alterações',
-        letterSize: 'Tamanho da letra',
-        color: 'Cor',
-        language: 'Idioma',
-        addLanguage: 'Adicionar idioma',
-        cancel: 'Cancelar',
-        add: 'Adicionar',
-      },
-    },
-  },
+  en: { translation: en },
+  es: { translation: es },
+  pt: { translation: pt },
 };
 
 // Obtener idioma inicial del usuario
